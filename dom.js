f7722dab92bbf7c9786b1acc0b707d0f87af3e42
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-04-20 17:33:42 +0530

    finally done

---
+++ dom.js
@@ -0,0 +1,22 @@
+// Watch uptill 23min - Make the title have a black border as the youtuber does
+// Now make ADD ITEM bold and chage the font color to greeen
+
+
+
+var headerTitle = document.getElementById("header-title");
+
+console.log(headerTitle);
+// headerTitle.textContent="Hello";
+
+// headerTitle.innerText="Goodbye";
+
+// 
+
+// headerTitle.innerHTML='<h3>Hello</h3>';
+
+headerTitle.style.border='solid 3px black'
+
+var addItem = document.getElementsByClassName("title");
+console.log(addItem)
+addItem[0].style.fontWeight="bold";
+addItem[0].style.color='green';
