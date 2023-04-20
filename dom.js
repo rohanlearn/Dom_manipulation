25c10c6e24f9c963657e5d2dd120574b033edb42
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-04-21 01:14:08 +0530

    tag name

--- dom.js
+++ dom.js
@@ -28,3 +28,11 @@ for (i=0;i<items.length;i++){
 }
 
 items[2].style.backgroundColor="green";
+
+// Add a new li element without the same class Name
+// And try editing it with getelementsbyclassname and then by getelementbytagname
+var li= document.getElementsByTagName("li");
+
+var lie=document.getElementsByClassName("list-r");
+
+lie[0].innerText="hi";
