e9cce75e4be568a233c1064137dcf9429596f54e
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-04-21 17:40:08 +0530

    new functionality

--- dom.js
+++ dom.js
@@ -1,53 +1,48 @@
-// Watch uptill 23min - Make the title have a black border as the youtuber does
-// Now make ADD ITEM bold and chage the font color to greeen
+var form = document.querySelector('#addForm');
+var itemList = document.querySelector('#items');
+var filter = document.getElementById('filter');
 
+form.addEventListener('submit',additem);
+itemList.addEventListener('click',removeItem);
+filter.addEventListener('keyup',filterItems);
 
-var itenList = document.querySelector("#items");
-console.log("current",itenList)
-// parentElement
-console.log("parentNode=",itenList.parentNode);
-itenList.parentNode.style.backgroundColor='red'
+function additem(event){
+    event.preventDefault();
+    var newItem = document.getElementById("item").value;
 
+    var li = document.createElement('li')
+    li.className="list-group-item";
+    li.appendChild(document.createTextNode(newItem));
+    var delButton = document.createElement('button');
+    delButton.className='btn btn-danger btn-sm float-right delete';
+    delButton.appendChild(document.createTextNode('X'));
+    li.appendChild(delButton);
+    
 
-//ChildNOdes
+    itemList.appendChild(li);
+}
 
-console.log("childnodes=",itenList.childNodes);
-// lastelementchild
-console.log("childnodes=lastElementChild",itenList.lastElementChild)
-// lastchild
-// createchild
-// firstelementchild
-// firstchild
-// nextsibling
-// nextelementsibling
-// previoussibling
-// previouselementsibling
-// createelement
-// setAttribute
-// createtesxtnode
-// appendchild
+function removeItem(event){
+    if (event.target.classList.contains('delete')){
+        if (confirm('Are You Sure?')){
+            var li= event.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
 
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    
+    var items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        console.log(itemName.toLowerCase(),text,itemName.toLowerCase().indexOf(text))
+        if(itemName.toLowerCase().indexOf(text)!=-1){
+            item.style.display='block';
+        }else{
+            item.style.display='none';
+        }
 
-
-
-// Now go head and add HEllo word before Item Lister
-var newDiv = document.createElement('div');
-
-var newDivText=document.createTextNode('HEllo word');
-
-newDiv.appendChild(newDivText);
-
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-
-
-
-container.insertBefore(newDiv,h1);
-
-// Now go head and add HEllo word before Item 1
-var list = document.querySelector(".list-group")
-var firstitem = list.firstChild
-
-list.insertBefore(newDiv,firstitem)
-// If stuck for long check the hints.
-
+    })
+}
