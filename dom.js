649a4288de4434298ba3106733c1f86fecb40f8b
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-04-21 22:07:50 +0530

    new functionality

--- dom.js
+++ dom.js
@@ -9,17 +9,31 @@ filter.addEventListener('keyup',filterItems);
 function additem(event){
     event.preventDefault();
     var newItem = document.getElementById("item").value;
+    var newDes = "   "+document.getElementById("desciption").value;
 
     var li = document.createElement('li')
     li.className="list-group-item";
     li.appendChild(document.createTextNode(newItem));
+    
+
     var delButton = document.createElement('button');
     delButton.className='btn btn-danger btn-sm float-right delete';
     delButton.appendChild(document.createTextNode('X'));
     li.appendChild(delButton);
+    li.appendChild(document.createElement("br"))
+    li.appendChild(document.createTextNode(newDes));
     
 
     itemList.appendChild(li);
+    var ipaut = document.getElementById('item');
+    
+    ipaut.value="Item "
+
+    var Des = document.getElementById("desciption");
+    Des.value=""
+
+    
+
 }
 
 function removeItem(event){
@@ -36,7 +50,9 @@ function filterItems(e){
     
     var items = itemList.getElementsByTagName('li');
     Array.from(items).forEach(function(item){
+        var itemdesc = item.lastChild.textContent
         var itemName = item.firstChild.textContent;
+        itemName=itemName+itemdesc
         console.log(itemName.toLowerCase(),text,itemName.toLowerCase().indexOf(text))
         if(itemName.toLowerCase().indexOf(text)!=-1){
             item.style.display='block';
